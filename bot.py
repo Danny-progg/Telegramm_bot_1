9753623ff6b3f91af4f51843a90343b787712414
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-09-22 21:15:25 +0300

    commit 1

---
+++ bot.py
@@ -0,0 +1,45 @@
+import telebot;
+bot = telebot.TeleBot('6022288398:AAF1qAQStNuqi2X36PGngfd3KM_jzXnk8eY');
+name = ''
+surname = ''
+age = 0
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    elif message.text == "/reg":
+        bot.send_message(message.from_user.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name)
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+def get_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message('Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+
+
+def get_age(message):
+    global age
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+            bot.send_message(message.from_user.id, 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?')
+
+
+bot.polling(none_stop=True, interval=0)
